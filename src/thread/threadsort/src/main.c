fbb026aaa6bc925f0a53a72688abf17241f144ac
m0sys <21181457+m0sys@users.noreply.github.com>
2023-03-14 13:12:22 -0400

    fix: make merging the sorted arrs work

--- src/thread/threadsort/src/main.c
+++ src/thread/threadsort/src/main.c
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 
 #define NUM_THREADS 3
-#define ARR_SZ 10
+#define ARR_SZ 11
 
 /* The global sorting variables to be shared amoung all threads. */
-int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+int arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
 /* Sorting algs. */
 void swap(int *arr, int i, int j) {
@@ -65,6 +65,8 @@ void *sort_worker(void *params) {
   p->sort_func(arr, p->start, p->end);
 
   /* Cleanly exit the worker. */
+  printf("sort_worker: start: %d => exiting ...\n", p->start);
+  print_arr(arr, p->start, p->end);
   free(params);
   pthread_exit(0);
 }
@@ -73,8 +75,11 @@ void *merge_worker(void *params) {
   merge_params *p = ((merge_params *)params);
 
   /* Only start merging once the two sorting threads are done. */
+  printf("merge_worker: waiting for sorting threads ...\n");
   pthread_join(p->tid1, NULL);
   pthread_join(p->tid2, NULL);
+  printf("merge_worker: done waiting for sorting threads!\n");
+  print_arr(arr, p->start1, p->end2);
 
   /* Create aux array. */
   int *aux = malloc(sizeof(int) * p->sz);
@@ -83,15 +88,17 @@ void *merge_worker(void *params) {
   for (int i = 0; i < p->sz; i++)
     aux[i] = arr[i];
 
+  print_arr(aux, p->start1, p->end2);
+
   for (int i = 0; i < p->sz; i++) {
-    if (p->start1 > p->end1) /* lhs fully merged */
+    if (p->start1 >= p->end1) /* lhs fully merged */
       arr[i] = aux[p->start2++];
-    else if (p->start2 > p->end2) /* rhs fully merged */
+    else if (p->start2 >= p->end2) /* rhs fully merged */
       arr[i] = aux[p->start1++];
-    else if (arr[p->start1] < arr[p->start2])
-      arr[i] = arr[p->start1++];
+    else if (aux[p->start1] < aux[p->start2])
+      arr[i] = aux[p->start1++];
     else
-      arr[i] = arr[p->start2++];
+      arr[i] = aux[p->start2++];
   }
 
   /* Cleanly exit the worker. */
@@ -113,9 +120,11 @@ int main(void) {
   int mid = ARR_SZ / 2;
 
   int start1 = 0;
-  int start2 = mid + 1;
+  int start2 = mid;
   int end1 = mid;
   int end2 = ARR_SZ;
+  printf("start1: %d, end1: %d\nstart2: %d, end2: %d\nmid: %d\n", start1, end1,
+         start2, end2, mid);
 
   /* lhs sorter */
   sort_params *sp = malloc(sizeof(sort_params));
@@ -143,8 +152,11 @@ int main(void) {
   pthread_create(&workers[2], &attr, merge_worker, mp);
 
   /* Wait on all threads before printing out the resulting sorted array. */
+
+  printf("main: waiting for all threads ...\n");
   for (int i = 0; i < NUM_THREADS; i++)
     pthread_join(workers[i], NULL);
+  printf("main: done waiting for all threads!\n");
 
   print_arr(arr, 0, ARR_SZ);
 }
