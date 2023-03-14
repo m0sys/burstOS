f795d894ed9ec094ba2894887e9bcc77b5faa1f3
m0sys <21181457+m0sys@users.noreply.github.com>
2023-03-14 12:34:09 -0400

    feat: add unleashing the threads! - broken though

--- src/thread/threadsort/src/main.c
+++ src/thread/threadsort/src/main.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM_THREADS 3
+#define ARR_SZ 10
+
+/* The global sorting variables to be shared amoung all threads. */
+int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
 /* Sorting algs. */
 void swap(int *arr, int i, int j) {
   int tmp = arr[i];
@@ -41,7 +47,6 @@ void print_arr(int *arr, int start, int end) {
 typedef struct {
   int start;
   int end;
-  int *arr;
   void (*sort_func)(int *, int, int);
 } sort_params;
 
@@ -51,48 +56,95 @@ typedef struct {
   int start2;
   int end2;
   int sz;
-  int *arr;
+  pthread_t tid1;
+  pthread_t tid2;
 } merge_params;
 
-void sort_worker(void *params) {
+void *sort_worker(void *params) {
   sort_params *p = ((sort_params *)params);
-  p->sort_func(p->arr, p->start, p->end);
+  p->sort_func(arr, p->start, p->end);
 
   /* Cleanly exit the worker. */
   free(params);
   pthread_exit(0);
 }
 
-void merge_worker(void *params) {
+void *merge_worker(void *params) {
   merge_params *p = ((merge_params *)params);
+
+  /* Only start merging once the two sorting threads are done. */
+  pthread_join(p->tid1, NULL);
+  pthread_join(p->tid2, NULL);
+
   /* Create aux array. */
   int *aux = malloc(sizeof(int) * p->sz);
 
   /* Copy the half sorted array into the aux array. */
   for (int i = 0; i < p->sz; i++)
-    aux[i] = p->arr[i];
+    aux[i] = arr[i];
 
   for (int i = 0; i < p->sz; i++) {
     if (p->start1 > p->end1) /* lhs fully merged */
-      p->arr[i] = aux[p->start2++];
+      arr[i] = aux[p->start2++];
     else if (p->start2 > p->end2) /* rhs fully merged */
-      p->arr[i] = aux[p->start1++];
-    else if (p->arr[p->start1] < p->arr[p->start2])
-      p->arr[i] = p->arr[p->start1++];
+      arr[i] = aux[p->start1++];
+    else if (arr[p->start1] < arr[p->start2])
+      arr[i] = arr[p->start1++];
     else
-      p->arr[i] = p->arr[p->start2++];
+      arr[i] = arr[p->start2++];
   }
 
   /* Cleanly exit the worker. */
+  free(aux);
   free(params);
   pthread_exit(0);
 }
 
 int main(void) {
   printf("Hello, World!\n");
-
-  int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-  print_arr(arr, 0, 10);
+  print_arr(arr, 0, ARR_SZ);
 
   /* Unleash the threads! */
+  pthread_t workers[NUM_THREADS];
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+
+  /* Init sorting threads. */
+  int mid = ARR_SZ / 2;
+
+  int start1 = 0;
+  int start2 = mid + 1;
+  int end1 = mid;
+  int end2 = ARR_SZ;
+
+  /* lhs sorter */
+  sort_params *sp = malloc(sizeof(sort_params));
+  sp->start = start1;
+  sp->end = end1;
+  sp->sort_func = insertion_sort;
+  pthread_create(&workers[0], &attr, sort_worker, sp);
+
+  /* rhs sorter */
+  sp = malloc(sizeof(sort_params));
+  sp->start = start2;
+  sp->end = end2;
+  sp->sort_func = insertion_sort;
+  pthread_create(&workers[1], &attr, sort_worker, sp);
+
+  /* Init merging thread. */
+  merge_params *mp = malloc(sizeof(merge_params));
+  mp->start1 = start1;
+  mp->end1 = end1;
+  mp->start2 = start2;
+  mp->end2 = end2;
+  mp->tid1 = workers[0];
+  mp->tid2 = workers[1];
+  mp->sz = ARR_SZ;
+  pthread_create(&workers[2], &attr, merge_worker, mp);
+
+  /* Wait on all threads before printing out the resulting sorted array. */
+  for (int i = 0; i < NUM_THREADS; i++)
+    pthread_join(workers[i], NULL);
+
+  print_arr(arr, 0, ARR_SZ);
 }
